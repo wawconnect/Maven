fccaa7b8caca9248d4c27ead756c20b7c5537737
call <call@ubuntu>
2017-07-24 01:04:13 +0100

    Maven

---
+++ HelloWorld/src/main/java/com/kdp/java/Greeting.java
@@ -0,0 +1,13 @@
+package com.kdp.java;
+
+/**
+ * @author Kshitij Pawar	
+ * @param args
+ */
+public class Greeting {
+
+	public String greet(String name){
+		return "Helloooooo "+ name + "###";
+	}
+
+}
---
+++ HelloWorld/src/main/java/com/kdp/java/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.kdp.java;
+
+public class HelloWorld {
+
+	/**
+	 * @author Kshitij Pawar	
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Greeting obj = new Greeting();
+		System.out.println(obj.greet("Jenkins"));
+	}	
+}
